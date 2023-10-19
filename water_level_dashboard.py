95a2dd777a62b3886fdbf2890ac21a5f79ab1cbe
keep-asdf <nusnugehy@gmail.com>
2023-10-20 06:40:55 +0900

    update

--- water_level_dashboard.py
+++ water_level_dashboard.py
@@ -1,21 +1,56 @@
 
+# # import streamlit as st
+# # from visualizations import *
+
+
+# # # Streamlit app
+# # def main():
+# #     st.title("Water Level Dashboard")
+
+# #     # 데이터를 로드합니다.
+# #     @st.cache_data
+# #     def load_data():
+# #         return pd.read_csv('data/water_level_with_moving_averages.csv').copy()  # 실제 데이터 경로로 바꾸세요.
+
+# #     data = load_data()
+
+# #     st.bokeh_chart(visualize_moving_averages_with_bokeh(data))
+# #     st.write(data)  # 데이터 프레임을 표로도 표시합니다.
+
+# # if __name__ == '__main__':
+# #     main()
 # import streamlit as st
 # from visualizations import *
-
+# import pandas as pd
 
 # # Streamlit app
 # def main():
 #     st.title("Water Level Dashboard")
 
-#     # 데이터를 로드합니다.
-#     @st.cache_data
-#     def load_data():
-#         return pd.read_csv('data/water_level_with_moving_averages.csv').copy()  # 실제 데이터 경로로 바꾸세요.
+#     # 사이드바를 사용하여 그래프 선택
+#     graph_selection = st.sidebar.selectbox("Choose a Graph", ["Moving Averages", "True vs Predicted with CI"])
 
-#     data = load_data()
+#     if graph_selection == "Moving Averages":
+#         # 데이터를 로드합니다.
+#         @st.cache_data
+#         def load_data_moving_averages():
+#             return pd.read_csv('data/water_level_with_moving_averages.csv').copy()
 
-#     st.bokeh_chart(visualize_moving_averages_with_bokeh(data))
-#     st.write(data)  # 데이터 프레임을 표로도 표시합니다.
+#         data_moving_averages = load_data_moving_averages()
+
+#         st.bokeh_chart(visualize_moving_averages_with_bokeh(data_moving_averages))
+#         st.write(data_moving_averages)
+
+#     elif graph_selection == "True vs Predicted with CI":
+#         # 데이터를 로드합니다.
+#         @st.cache_data
+#         def load_data_true_pred():
+#             return pd.read_csv('data/true_pred_with_CI.csv').copy()
+
+#         data_true_pred = load_data_true_pred()
+
+#         st.bokeh_chart(visualize_true_pred_with_CI_and_status_lines_bokeh(data_true_pred))
+#         st.write(data_true_pred)
 
 # if __name__ == '__main__':
 #     main()
@@ -27,8 +62,15 @@ import pandas as pd
 def main():
     st.title("Water Level Dashboard")
 
+    # 사이드바에 버튼을 추가합니다.
+    update_button = st.sidebar.button("Update Data")
+
     # 사이드바를 사용하여 그래프 선택
     graph_selection = st.sidebar.selectbox("Choose a Graph", ["Moving Averages", "True vs Predicted with CI"])
+
+    # 만약 버튼이 클릭되면, 데이터를 다시 로드하고 그래프와 표를 다시 그립니다.
+    if update_button:
+        st.caching.clear_cache()
 
     if graph_selection == "Moving Averages":
         # 데이터를 로드합니다.
