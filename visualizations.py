a851b476d0faf02a2122e78e5dd92d1df890858c
keep-asdf <nusnugehy@gmail.com>
2023-10-20 05:28:27 +0900

    update

--- visualizations.py
+++ visualizations.py
@@ -1,17 +1,13 @@
   
-
 import pandas as pd
-from bokeh.plotting import figure, show, output_notebook
-from bokeh.io import push_notebook
-from bokeh.models import ColumnDataSource, Band, Legend, HoverTool
+from bokeh.plotting import figure
+from bokeh.models import ColumnDataSource, Band, HoverTool
 from bokeh.palettes import Category10
-from bokeh.layouts import column
 
 def visualize_moving_averages_with_bokeh(dataframe):
     dataframe['Time'] = pd.to_datetime(dataframe['Time'])
 
-    p = figure(x_axis_type="datetime", width=800, height=400, 
-               title="Predicted MHC Water Level with Confidence Intervals, Moving Averages and Status Lines")
+    p = figure(x_axis_type="datetime", width=800, height=400, title="Predicted MHC Water Level with Confidence Intervals, Moving Averages and Status Lines")
     source = ColumnDataSource(dataframe)
 
     # Plot the Predicted Water Level
@@ -28,26 +24,21 @@ def visualize_moving_averages_with_bokeh(dataframe):
             color = next(ma_colors)
             ma_line = p.line('Time', column, source=source, color=color, legend_label=column)
     
-    # Create a new source for the status lines
-    status_data = {
-        'Time': [dataframe.Time.min(), dataframe.Time.max()],
+    # Adding the status lines
+    p.line([dataframe.Time.min(), dataframe.Time.max()], [9.2, 9.2], color='red', line_dash="dashed", legend_label="Severe")
+    p.line([dataframe.Time.min(), dataframe.Time.max()], [8.0, 8.0], color='orange', line_dash="dashed", legend_label="Alert")
+    p.line([dataframe.Time.min(), dataframe.Time.max()], [7.0, 7.0], color='yellow', line_dash="dashed", legend_label="Caution")
+    p.line([dataframe.Time.min(), dataframe.Time.max()], [5.0, 5.0], color='green', line_dash="dashed", legend_label="Attention")
 
-    }
-    status_source = ColumnDataSource(status_data)
-
-    # Adding the status lines using the status source
-    p.line('Time', 'Severe',  color='red', line_dash="dashed", legend_label="Severe")
-    p.line('Time', 'Alert',  color='orange', line_dash="dashed", legend_label="Alert")
-    p.line('Time', 'Caution',  color='yellow', line_dash="dashed", legend_label="Caution")
-    p.line('Time', 'Attention',  color='green', line_dash="dashed", legend_label="Attention")
-
-    # Modify the hover tool to display information for each status line
+    p.legend.location = "top_left"
+    p.legend.click_policy="hide"
+    
+    # Modify the hover tool to only display information for the main line and confidence interval
     hover = HoverTool(
         tooltips=[
             ("Time", "@Time{%F %T}"),
             ("Water Level", "@Predicted_MHC_Water_Level"),
-            ("Confidence Interval", "(@CI_Lower, @CI_Upper)"),
-
+            ("Confidence Interval", "(@CI_Lower, @CI_Upper)")
         ],
         formatters={
             "@Time": "datetime"
@@ -57,5 +48,4 @@ def visualize_moving_averages_with_bokeh(dataframe):
     p.add_tools(hover)
 
     return p
-
 
