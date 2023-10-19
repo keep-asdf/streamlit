bc31b87c7d1b6d510652d3ec7b325e3ae87fc45f
keep-asdf <nusnugehy@gmail.com>
2023-10-20 05:53:55 +0900

    update

--- .ipynb_checkpoints/visualizations-checkpoint.py
+++ .ipynb_checkpoints/visualizations-checkpoint.py
@@ -6,6 +6,7 @@ from bokeh.palettes import Category10
 from bokeh.layouts import column
 
 def visualize_moving_averages_with_bokeh(dataframe):
+    
     dataframe['Time'] = pd.to_datetime(dataframe['Time'])
 
     p = figure(x_axis_type="datetime", width=800, height=400, 
@@ -42,6 +43,7 @@ def visualize_moving_averages_with_bokeh(dataframe):
     # Modify the hover tool to display information for each status line
     hover = HoverTool(
         tooltips=[
+            
             ("Time", "@Time{%F %T}"),
             ("Water Level", "@Predicted_MHC_Water_Level"),
             ("Confidence Interval", "(@CI_Lower, @CI_Upper)"),
@@ -57,60 +59,42 @@ def visualize_moving_averages_with_bokeh(dataframe):
     return p
 
 
-# import pandas as pd
-# from bokeh.plotting import figure, show, output_notebook
-# from bokeh.io import push_notebook
-# from bokeh.models import ColumnDataSource, Band, Legend, HoverTool
-# from bokeh.palettes import Category10
-# from bokeh.layouts import column
+def visualize_true_pred_with_CI_and_status_lines_bokeh(dataframe):
+    dataframe['Time'] = pd.to_datetime(dataframe['Time'])
+    source = ColumnDataSource(dataframe)
 
-# def visualize_moving_averages_with_bokeh(dataframe):
-#     dataframe['Time'] = pd.to_datetime(dataframe['Time'])
+    p = figure(x_axis_type="datetime", width=800, height=400, 
+               title="True vs Predicted Values with Confidence Intervals and Status Lines")
 
-#     p = figure(x_axis_type="datetime", width=800, height=400, 
-#                title="Predicted MHC Water Level with Confidence Intervals, Moving Averages and Status Lines",
-#                legend_label_text_font_size="85%")  # <-- Adjust the legend label font size here
+    # Plot the True Values
+    p.line('Time', 'True_Value', source=source, color="green", legend_label="True Value")
 
-#     source = ColumnDataSource(dataframe)
+    # Plot the Predicted Values
+    p.line('Time', 'Predicted_Value', source=source, color="blue", legend_label="Predicted Value")
 
-#     # Plot the Predicted Water Level
-#     line = p.line('Time', 'Predicted_MHC_Water_Level', source=source, color="blue", legend_label="Predicted Water Level")
+    # Plot confidence intervals
+    band = Band(base='Time', lower='CI_Lower', upper='CI_Upper', source=source, level='underlay', fill_alpha=0.3, fill_color='blue')
+    p.add_layout(band)
 
-#     # Plot confidence intervals
-#     band = Band(base='Time', lower='CI_Lower', upper='CI_Upper', source=source, level='underlay', fill_alpha=0.3, fill_color='blue')
-#     p.add_layout(band)
+    # Adding the status lines
+    p.line([dataframe.Time.min(), dataframe.Time.max()], [9.2, 9.2], color='red', line_dash="dashed", legend_label="Severe")
+    p.line([dataframe.Time.min(), dataframe.Time.max()], [8.0, 8.0], color='orange', line_dash="dashed", legend_label="Alert")
+    p.line([dataframe.Time.min(), dataframe.Time.max()], [7.0, 7.0], color='yellow', line_dash="dashed", legend_label="Caution")
+    p.line([dataframe.Time.min(), dataframe.Time.max()], [5.0, 5.0], color='green', line_dash="dashed", legend_label="Attention")
 
-#     # Plot the moving averages
-#     ma_colors = iter(Category10[10])
-#     for column in dataframe.columns:
-#         if "MA" in column:
-#             color = next(ma_colors)
-#             ma_line = p.line('Time', column, source=source, color=color, legend_label=column)
-    
-#     # Create a new source for the status lines
-#     status_data = {
-#         'Time': [dataframe.Time.min(), dataframe.Time.max()]
-#     }
-#     status_source = ColumnDataSource(status_data)
+    # Hover tool
+    hover = HoverTool(
+        tooltips=[
+            ("Time", "@Time{%F %T}"),
+            ("True Value", "@True_Value"),
+            ("Predicted Value", "@Predicted_Value"),
+            ("Confidence Interval", "(@CI_Lower, @CI_Upper)")
+        ],
+        formatters={
+            "@Time": "datetime"
+        },
+        mode='mouse'
+    )
+    p.add_tools(hover)
 
-#     # Adding the status lines using the status source
-#     p.line('Time', 9.2, source=status_source, color='red', line_dash="dashed", legend_label="Severe")
-#     p.line('Time', 8.0, source=status_source, color='orange', line_dash="dashed", legend_label="Alert")
-#     p.line('Time', 7.0, source=status_source, color='yellow', line_dash="dashed", legend_label="Caution")
-#     p.line('Time', 5.0, source=status_source, color='green', line_dash="dashed", legend_label="Attention")
-
-#     # Modify the hover tool to display information for each status line
-#     hover = HoverTool(
-#         tooltips=[
-#             ("Time", "@Time{%F %T}"),
-#             ("Water Level", "@Predicted_MHC_Water_Level"),
-#             ("Confidence Interval", "(@CI_Lower, @CI_Upper)")
-#         ],
-#         formatters={
-#             "@Time": "datetime"
-#         },
-#         mode='mouse'
-#     )
-#     p.add_tools(hover)
-
-#     return p
+    return p
--- water_level_dashboard.py
+++ water_level_dashboard.py
@@ -1,42 +1,56 @@
+
 # import streamlit as st
-# import pandas as pd
-# from visualizations import visualize_moving_averages_with_cross_highlighted
-
-# # 데이터를 로드합니다.
-# @st.cache
-# def load_data():
-#     return pd.read_csv('data/water_level_with_moving_averages.csv')  # 실제 데이터 경로로 바꾸세요.
-
-# data = load_data()
-
-# st.title("Water Level Dashboard")
-
-# # 데이터 프레임을 그래프로 표시합니다.
-# st.write("Predicted MHC Water Level Visualization")
-
-# # 데이터의 복사본을 사용하여 시각화 함수를 호출합니다.
-# visualize_moving_averages_with_cross_highlighted(data.copy())
-
-# st.write(data)  # 데이터 프레임을 표로도 표시합니다.
+# from visualizations import *
 
 
+# # Streamlit app
+# def main():
+#     st.title("Water Level Dashboard")
+
+#     # 데이터를 로드합니다.
+#     @st.cache_data
+#     def load_data():
+#         return pd.read_csv('data/water_level_with_moving_averages.csv').copy()  # 실제 데이터 경로로 바꾸세요.
+
+#     data = load_data()
+
+#     st.bokeh_chart(visualize_moving_averages_with_bokeh(data))
+#     st.write(data)  # 데이터 프레임을 표로도 표시합니다.
+
+# if __name__ == '__main__':
+#     main()
 import streamlit as st
 from visualizations import *
-
+import pandas as pd
 
 # Streamlit app
 def main():
     st.title("Water Level Dashboard")
 
-    # 데이터를 로드합니다.
-    @st.cache_data
-    def load_data():
-        return pd.read_csv('data/water_level_with_moving_averages.csv').copy()  # 실제 데이터 경로로 바꾸세요.
+    # 사이드바를 사용하여 그래프 선택
+    graph_selection = st.sidebar.selectbox("Choose a Graph", ["Moving Averages", "True vs Predicted with CI"])
 
-    data = load_data()
+    if graph_selection == "Moving Averages":
+        # 데이터를 로드합니다.
+        @st.cache
+        def load_data_moving_averages():
+            return pd.read_csv('data/water_level_with_moving_averages.csv').copy()
 
-    st.bokeh_chart(visualize_moving_averages_with_bokeh(data))
-    st.write(data)  # 데이터 프레임을 표로도 표시합니다.
+        data_moving_averages = load_data_moving_averages()
+
+        st.bokeh_chart(visualize_moving_averages_with_bokeh(data_moving_averages))
+        st.write(data_moving_averages)
+
+    elif graph_selection == "True vs Predicted with CI":
+        # 데이터를 로드합니다.
+        @st.cache
+        def load_data_true_pred():
+            return pd.read_csv('data/true_pred_with_CI.csv').copy()
+
+        data_true_pred = load_data_true_pred()
+
+        st.bokeh_chart(visualize_true_pred_with_CI_and_status_lines_bokeh(data_true_pred))
+        st.write(data_true_pred)
 
 if __name__ == '__main__':
     main()
