3f5fae5e173dfe34228286a098c99585ff19b59c
keep-asdf <nusnugehy@gmail.com>
2023-10-19 20:46:57 +0900

    code update

--- water_level_dashboard.py
+++ water_level_dashboard.py
@@ -1,8 +1,10 @@
+import streamlit as st
 import pandas as pd
 import plotly.express as px
-import streamlit as st
 
-    
+# 페이지 설정: 페이지 제목 및 레이아웃 설정
+st.set_page_config(page_title="Water Level Predictions", layout="wide")
+
 # 앱의 타이틀 설정
 st.title("Water Level Predictions with Confidence Interval")
 
@@ -19,6 +21,13 @@ fig.add_scatter(x=data['Time'], y=data['CI_Lower'], mode='lines', name='CI Lower
 fig.add_scatter(x=data['Time'], y=data['CI_Upper'], mode='lines', name='CI Upper')
 
 st.plotly_chart(fig)
-    
-    
 
+# 어두운 테마 적용을 위한 CSS
+st.markdown("""
+<style>
+body {
+    color: #fff;
+    background-color: #4F4F4F;
+}
+</style>
+    """, unsafe_allow_html=True)
