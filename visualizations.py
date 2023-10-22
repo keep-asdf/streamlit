98e8c2e5f9793ca42bd817d3973cac1308304f1e
keep-asdf <nusnugehy@gmail.com>
2023-10-22 20:06:35 +0900

    'update'

--- visualizations.py
+++ visualizations.py
@@ -147,7 +147,7 @@ def visualize_true_pred_with_CI_and_status_lines_bokeh(dataframe):
 #     p.legend.visible = False
     
 #     return p
-def visualize_last_6h_moving_averages_adjusted(data):
+def visualize_last_6h_moving_averages(data):
     # 데이터를 ColumnDataSource로 변환
     source = ColumnDataSource(data)
 
