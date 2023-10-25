c839ed7b9c7b7dc05de127b4aa63eab98974cc5d
keep-asdf <nusnugehy@gmail.com>
2023-10-25 18:16:40 +0900

    'update'

--- .ipynb_checkpoints/water_level_dashboard-checkpoint.py
+++ .ipynb_checkpoints/water_level_dashboard-checkpoint.py
@@ -237,7 +237,7 @@ def main():
             view_state = pdk.ViewState(
                 latitude=36.6230541816206,
                 longitude=127.35070148286204 ,
-                zoom=15,
+                zoom=14,
                 pitch=0
                         )
 
