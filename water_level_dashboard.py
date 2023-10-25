0e101e2d7f1c657a13b15adc4b5c2a86209379b9
keep-asdf <nusnugehy@gmail.com>
2023-10-25 19:34:07 +0900

    'update'

--- water_level_dashboard.py
+++ water_level_dashboard.py
@@ -144,7 +144,7 @@ def main():
                 st.write(traffic_df)
             
             with col2:
-                st.map(traffic_df,latitude = 'coordX', longitude = 'coordY', color = '#8B0000')
+                st.map(traffic_df,latitude = 'coordX', longitude = 'coordY', color = '#8B0000', size = 10)
                 
 #             st.subheader("실시간 미호천교 근방 교통 이벤트 데이터(empty = 현재 이벤트 없음)")
 #             st.write(traffic_data)
