589b513a7c6d298c061c65689ce8e62b032ddafa
keep-asdf <nusnugehy@gmail.com>
2024-08-06 14:36:42 +0900

    'update'

--- water_level_dashboard.py
+++ water_level_dashboard.py
@@ -264,9 +264,9 @@ def main():
             
             st.bokeh_chart(plot_predictions_with_uncertainty_bokeh(pred_uncer)
             
-            data_true_pred = load_data_pred_uncer()
+#             data_true_pred = load_data_pred_uncer()
 
-            data_true_pred['Time'] = pd.to_datetime(data_true_pred['Time'])
+#             data_true_pred['Time'] = pd.to_datetime(data_true_pred['Time'])
             
             
             
