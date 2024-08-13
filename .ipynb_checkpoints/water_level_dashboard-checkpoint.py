d5777318f533c13a14a7d7cfe651c55980d2a81f
keep-asdf <nusnugehy@gmail.com>
2024-08-13 16:59:32 +0900

    'update'

--- .ipynb_checkpoints/water_level_dashboard-checkpoint.py
+++ .ipynb_checkpoints/water_level_dashboard-checkpoint.py
@@ -342,9 +342,11 @@ def main():
 
     # Email Notification 페이지
     elif choice == "Email Notification":
+        
         # 여백 추가 (첫 번째 방법: st.write("") 사용)
         st.write("")  # 한 줄의 여백 추가    
         st.write("")  # 한 줄의 여백 추가    
+        
         st.subheader("이메일로 위험 알림 받기")
     
         email_address = st.text_input('이메일 주소를 입력해주세요', 
